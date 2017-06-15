8608fe5abaeb8b10e098a46137bdf8ce8b893aa6
Simon Murray <simon.murray@datacentred.co.uk>
2017-06-15 13:29:35 +0100

    Force Systemctl Reload
    On multiple occasions now when provisioning OSDs I've seen devices not have the
    ceph:ceph udev rules applied and things got wuite wrong.  The online fix appears
    to be a reload of systemctl, so force this once we've installed and enabled the
    ceph target.  This is a strictly pre-emptive action, with no guarantees of working

--- manifests/service.pp
+++ manifests/service.pp
@@ -8,6 +8,13 @@ class ceph::service {
     exec { 'ceph.target enable':
       command => '/bin/systemctl enable ceph.target',
       unless  => '/bin/systemctl is-enabled ceph.target',
+    } ~>
+
+    # Oddly I've seen OSD udev rules not applying on Xenial which are
+    # fixed with a reload
+    exec { 'ceph::service systemctl reload':
+      command     => '/bin/systemctl daemon-reload',
+      refreshonly => true,
     }
   }
 
