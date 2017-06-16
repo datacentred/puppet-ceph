c854ff7f0557147d86bf0eca61872374b6842c07
Simon Murray <simon.murray@datacentred.co.uk>
2017-06-16 10:26:20 +0100

    Update EPEL to 7.9
    Required for CI tests for Centos 7

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -20,7 +20,7 @@ RSpec.configure do |c|
 
       # Install EPEL on centos
       if host['platform'].start_with?('el')
-        on(host, 'rpm -i http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm')
+        on(host, 'rpm -i http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm')
       end
     end
   end
